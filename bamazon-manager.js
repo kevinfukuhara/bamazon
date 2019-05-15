8622a2356c483efc64f915ef6a274a66ec459653
Fukuhara <Kevin.Fukuhara2@t-mobile.com>
2019-05-15 11:03:06 -0700

    got Manager GUI to add stock to an item

--- bamazon-manager.js
+++ bamazon-manager.js
@@ -42,8 +42,10 @@ function managerPrompt() {
                     displayProducts('low');
                     break;
                 case 'Add to Inventory.':
+                    addInventory();
                     break;
                 case 'Add New Product.':
+                
                     break;
                 default:
                     console.log("Thank you, have a nice day!");
@@ -58,7 +60,7 @@ function displayProducts(queryScope) {
     var query;
 
     // Generate the correct query based on what results we want displayed
-    if(queryScope === 'all') {
+    if (queryScope === 'all') {
         query = 'SELECT * FROM products';
     } else if (queryScope === 'low') {
         query = 'SELECT * FROM products WHERE stock_quantity < 5';
@@ -76,51 +78,80 @@ function displayProducts(queryScope) {
 
 function addInventory() {
     inquirer
-    .prompt([
-        // Here we give the user a list to choose from.
-        {
-            type: "input",
-            message: "Please enter the 'item_id' of the product you would like to add inventory for?",
-            validate: function(ans) {
-                return (typeof ans === 'number');
+        .prompt([
+            // Here we give the user a list to choose from.
+            {
+                type: "input",
+                message: "Please enter the 'item_id' of the product you would like to add inventory for?",
+                // validate: function(ans) {
+                //     return (typeof ans === 'number');
+                // },
+                name: "item"
             },
-            name: "item"
-        },
-        {
-            type: "input",
-            message: "How much inventory would you like to add?",
-            validate: function(ans) {
-                return (typeof ans === 'number');
-            },
-            name: "stockAdd"
-        }
-    ])
-    .then(function (inquirerResponse) {
-        // Query Database for the item of id from user input
-        var query = "SELECT * FROM products WHERE item_id=" + inquirerResponse.item;
-        console.log("Searching for your tiem with item_id: " + inquirerResponse.item);
-        connection.query(query, function (err, res) {
-            if (err) throw err;
-
-            // Validate whether or not the requested item is valid
-            if(res[0].length === 0) {
-                // Not a valid product case
-                console.log("Not a valid item please try again.\n");
-                managerPrompt();
-            } else {
-                // Valid Product case
-                console.log("Valid product.\n" +
-                "item_id: " + res[0].item_id + 
-                "product_name: " + res[0].product_name + 
-                "stock_quantity (original): " + res[0].stock_quantity + "\n");
-
-                console.log("Updating the quantity now... Adding " + inquirerResponse.stockAdd + " to the stock's quantity.")
-
-                connection.end();
+            {
+                type: "input",
+                message: "How much inventory would you like to add?",
+                // validate: function(ans) {
+                //     return (typeof ans === 'number');
+                // },
+                name: "stockAdd"
             }
-        });
+        ])
+        .then(function (inquirerResponse) {
+            // Query Database for the item of id from user input
+            var query = "SELECT * FROM products WHERE item_id=" + inquirerResponse.item;
+            console.log("Searching for your item with item_id: " + inquirerResponse.item);
+            connection.query(query, function (err, res) {
+                if (err) throw err;
 
-    });
+                // Validate whether or not the requested item is valid
+                if (res[0].length === 0) {
+                    // Not a valid product case
+                    console.log("Not a valid item please try again.\n");
+                    managerPrompt();
+                } else {
+                    // Valid Product case
+                    console.log("Valid product.\n" +
+                        "item_id: " + res[0].item_id + "\n" +
+                        "product_name: " + res[0].product_name + "\n" +
+                        "stock_quantity (original): " + res[0].stock_quantity + "\n");
+
+                    console.log("Updating the quantity now... Adding " + inquirerResponse.stockAdd + " to the stock's quantity.");
+
+                    // connection.end();
+
+                    var newStock = res[0].stock_quantity + parseInt(inquirerResponse.stockAdd);
+
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newStock
+                            },
+                            {
+                                item_id: res[0].item_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw error;
+
+                            console.log("Item successfully updated! Printing results now... \n");
+
+                            var query = "SELECT * FROM products WHERE item_id=" + res[0].item_id;
+                            
+                            connection.query(query, function (err, res) {
+                                if (err) throw err;
+
+                                console.table(res);
+
+                                // Back to beginning
+                                managerPrompt();
+                            });
+                        });
+                    }
+            });
+
+        });
 }
 
 
